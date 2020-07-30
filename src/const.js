93f7476ba8041abc853c04c3360c8b7ff509fb47
anthowen <webtal.blizzard@gmail.com>
2020-07-30 15:20:21 -0400

    Trying ReactHighcharts demo

---
+++ src/const.js
@@ -0,0 +1,11 @@
+export const API_URL = 'https://stage.altrac-api.com/evapo/address/';
+
+export const DEVICE_ID = '26002e000c51343334363138';
+
+export const TZ_OFFSET = -7;
+
+export const ELEVATION = 160.9;
+
+export const LATITUDE = 43.2624613;
+
+export const EVA_COEF = 0.51;
